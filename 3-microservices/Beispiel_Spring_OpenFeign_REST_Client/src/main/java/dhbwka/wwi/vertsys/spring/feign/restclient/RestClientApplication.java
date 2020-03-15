7fcae240f60206d41ae2b5876a0db454c5225d0f
Dennis Schulmeister-Zimolong <dennis@patk.org>
2020-03-15 22:58:41 +0100

    Rest-Client bei Unit Tests nicht ausführen

--- 3-microservices/Beispiel_Spring_OpenFeign_REST_Client/src/main/java/dhbwka/wwi/vertsys/spring/feign/restclient/RestClientApplication.java
+++ 3-microservices/Beispiel_Spring_OpenFeign_REST_Client/src/main/java/dhbwka/wwi/vertsys/spring/feign/restclient/RestClientApplication.java
@@ -12,23 +12,29 @@ package dhbwka.wwi.vertsys.spring.feign.restclient;
 import dhbwka.wwi.vertsys.spring.feign.restclient.SongListProxy.Message;
 import dhbwka.wwi.vertsys.spring.feign.restclient.SongListProxy.Song;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Component;
 
 /**
  * Hauptklasse der Anwendung. Diese Anwendung konsumiert den in der Anwendung
  * "Beispiel Spring OpenFeign (REST-Server)" bereitgestellten Webservice.
+ * 
+ * Zur Annotation @ConditionalOnProperty vgl.
+ * https://stackoverflow.com/a/45036625. Sie dient dazu, dass die run()-Methode
+ * nicht in den Unit Tests der Anwendung ausgeführt wird.
  */
 @SpringBootApplication
 @EnableFeignClients
 @Component
-public class RestClientApplication {
+@ConditionalOnProperty(prefix = "job.autorun", name = "enabled", havingValue = "true", matchIfMissing = true)
+public class RestClientApplication implements CommandLineRunner {
 
     private static Logger logger = LoggerFactory.getLogger(RestClientApplication.class);
 
@@ -39,8 +45,8 @@ public class RestClientApplication {
         SpringApplication.run(RestClientApplication.class, args);
     }
 
-    @PostConstruct
-    private void callWebservice() {
+    @Override
+    public void run(String... args) {
         logger.info("");
         logger.info("");
         logger.info(">>> ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
--- 3-microservices/Beispiel_Spring_OpenFeign_REST_Client/src/test/java/dhbwka/wwi/vertsys/spring/feign/restclient/RestclientApplicationTests.java
+++ 3-microservices/Beispiel_Spring_OpenFeign_REST_Client/src/test/java/dhbwka/wwi/vertsys/spring/feign/restclient/RestclientApplicationTests.java
@@ -3,11 +3,11 @@ package dhbwka.wwi.vertsys.spring.feign.restclient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"job.autorun.enabled=false"})
 class RestclientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
